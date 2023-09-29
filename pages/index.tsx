86f69b9fcc8ff07876721632db56095388e22944
Kabilan <kumarakabilan2004@gmail.com>
2023-09-29 12:28:10 +0530

    First

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,108 +1,49 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, MediaRenderer, useContract, useContractMetadata, useMetadata } from "@thirdweb-dev/react";
+import {Box, Container, Flex, Heading, SimpleGrid, Skeleton, Text} from "@chakra-ui/react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import { NextPage } from "next";
+import { useState } from "react";
+
 
 const Home: NextPage = () => {
-  return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
 
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
+  const {
+    contract
+  } = useContract("0x76dC6e5BB04006876Ee8F56383D9c628cBBbe10d");
 
-          <div className={styles.connect}>
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
-              }}
-            />
-          </div>
-        </div>
+  const {
+    data: contractMetadata,
+    isLoading: isContractMetadataLoading,
+  } = useContractMetadata(contract);
 
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
-      </div>
-    </main>
+    return (
+    <Container maxW={"1200px"}>
+      <Flex p={"20px"} justifyContent={"space-between"}>
+        <Box></Box>
+        <ConnectWallet/>
+      </Flex>
+      <Flex h={"90vh"} alignItems={"center"} justifyContent={"center"}>
+        <SimpleGrid columns={2} spacing={10} justifyContent={"center"}>
+          <Box>
+          {!isContractMetadataLoading && (
+            <Skeleton isLoaded={!isContractMetadataLoading}>
+            <MediaRenderer
+                src={contractMetadata.image}
+              />
+            </Skeleton>
+          )}
+          </Box>
+          <Flex>
+          {!isContractMetadataLoading && (
+            <Skeleton isLoaded={!isContractMetadataLoading}>
+            <Heading>{contractMetadata.name}</Heading>
+            </Skeleton>
+          )}
+          </Flex>
+        </SimpleGrid>
+      </Flex>
+    </Container>
   );
 };
 
